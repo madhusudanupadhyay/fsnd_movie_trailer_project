7720d2d7f45b7fcfe20d11d8903172d46ca83dfb
madhusudanupadhyay <upadhyay.ms01@gmail.com>
2017-03-02 13:57:35 +0530

    Dismissed modal on-click

--- assets/main.js
+++ assets/main.js
@@ -14,3 +14,11 @@ $(document).on('click', '.movie-tile', function(event) {
         'frameborder': 0
     }));
 });
+
+// Pause the video when the modal is closed
+$(document).on('click', '.hanging-close, .modal-backdrop, .modal', function(event) {
+    // Remove the src so the player itself gets removed, as this is the only
+    // reliable way to ensure the video stops playing in IE
+    $("#trailer-video-container").empty();
+    $("#trailer").css("display", "none");
+});
