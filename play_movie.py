fae9c2b62c2466c644d7082827740b7c5152b51e
madhusudanupadhyay <upadhyay.ms01@gmail.com>
2017-03-02 15:48:17 +0530

    Make code pep8 compatible

--- play_movie.py
+++ play_movie.py
@@ -10,9 +10,13 @@ API_KEY = 'a6858133c502c06591520aeea294f20b'
 
 UPCOMING_URL = '%s/movie/upcoming?api_key=%s' % (BASE_URL, API_KEY)
 
+
 def movie_details_url(movie_id):
     """ Returns API url for getting movie details """
-    return '%s/movie/%s?api_key=%s&append_to_response=videos' % (BASE_URL, movie_id, API_KEY)
+    return '%s/movie/%s?api_key=%s&append_to_response=videos' % (
+        BASE_URL, movie_id, API_KEY
+    )
+
 
 def upcoming_movie_ids():
     """ Get upcoming movie ids from the API """
@@ -21,13 +25,16 @@ def upcoming_movie_ids():
     ids = [movie_obj['id'] for movie_obj in movies]
     return ids
 
+
 def get_movie_model(api_url):
     """ Make movie model from url """
     res = requests.get(api_url).json()
     title = res['title'].encode('ascii', 'ignore')
     storyline = res['overview'].encode('ascii', 'ignore')
     yt_code = res['videos']['results'][0]['key'].encode('ascii', 'ignore')
-    poster = 'https://image.tmdb.org/t/p/w500/' + res['poster_path'].encode('ascii', 'ignore')
+    poster = 'https://image.tmdb.org/t/p/w500/' + res['poster_path'].encode(
+        'ascii', 'ignore'
+    )
 
     return movie.Movie(title, storyline, yt_code, poster)
 
